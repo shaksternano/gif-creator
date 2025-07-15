32ce42c4daaead704aa08757e9e05d3799ca487c
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-15 19:39:32 +0100

    feat: move output to the right and add progress bar

--- site/src/jsMain/kotlin/pages/Index.kt
+++ site/src/jsMain/kotlin/pages/Index.kt
@@ -13,16 +13,17 @@ import com.shakster.gifcreator.worker.GifWorkerInput
 import com.shakster.gifcreator.worker.GifWorkerOutput
 import com.shakster.gifkt.GIF_MINIMUM_FRAME_DURATION_CENTISECONDS
 import com.shakster.gifkt.centiseconds
-import com.varabyte.kobweb.compose.css.Cursor
-import com.varabyte.kobweb.compose.css.Overflow
+import com.varabyte.kobweb.compose.css.*
 import com.varabyte.kobweb.compose.css.Transition
-import com.varabyte.kobweb.compose.css.autoLength
+import com.varabyte.kobweb.compose.css.functions.LinearGradient
+import com.varabyte.kobweb.compose.css.functions.linearGradient
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.compose.ui.toAttrs
@@ -30,6 +31,8 @@ import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.forms.Button
 import com.varabyte.kobweb.silk.components.forms.Input
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.fa.FaArrowRight
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
 import com.varabyte.kobweb.worker.Attachments
 import kotlinx.browser.window
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -48,12 +51,11 @@ import org.w3c.files.Blob
 import org.w3c.files.BlobPropertyBag
 import org.w3c.files.File
 import kotlin.math.max
-import kotlin.time.Clock
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
-import kotlin.time.ExperimentalTime
+import kotlin.time.DurationUnit
+import kotlin.time.TimeSource
 
-@OptIn(ExperimentalTime::class)
 @Page
 @Composable
 fun HomePage() {
@@ -64,19 +66,17 @@ fun HomePage() {
 
     var encoding by remember { mutableStateOf(false) }
 
-    var startTime by remember { mutableStateOf(Clock.System.now()) }
+    var startTime by remember { mutableStateOf(TimeSource.Monotonic.markNow()) }
     var elapsedTime by remember { mutableStateOf(0.seconds) }
     var encodedFrames by remember { mutableStateOf(0) }
-    var averageFps by remember { mutableStateOf(0.0) }
-    var maxFps by remember { mutableStateOf(0.0) }
+    var fps by remember { mutableStateOf(0.0) }
 
     val messageChannel = remember {
         val channel = MessageChannel()
         channel.port1.onmessage = { event ->
             val frameEvent = Json.decodeFromString<GifFrameWrittenEvent>(event.data as String)
             encodedFrames = frameEvent.framesWritten
-            averageFps = frameEvent.framesWritten / (elapsedTime.inWholeMilliseconds / 1000.0)
-            maxFps = max(maxFps, averageFps)
+            fps = frameEvent.framesWritten / (elapsedTime.inWholeMilliseconds / 1000.0)
         }
         channel
     }
@@ -99,7 +99,8 @@ fun HomePage() {
 
         window.setInterval({
             if (encoding) {
-                elapsedTime = Clock.System.now() - startTime
+                val currentTime = TimeSource.Monotonic.markNow()
+                elapsedTime = currentTime - startTime
             }
         })
     }
@@ -117,129 +118,35 @@ fun HomePage() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            var highlight by remember { mutableStateOf(false) }
-
-            FileDropBox(
+            Row(
                 Modifier
-                    .width(80.percent)
-                    .minWidth(300.px)
-                    .maxWidth(1200.px)
+                    .fillMaxWidth()
                     .height(80.percent)
                     .minHeight(300.px)
-                    .borderRadius(10.px)
-                    .padding(30.px)
-                    .backgroundColor(Color.gray)
-                    .outline(width = 3.px, style = LineStyle.Dashed, color = Color.black)
-                    .outlineOffset((-10).px)
-                    .display(DisplayStyle.Flex)
-                    .overflow(Overflow.Auto)
-                    .transition(Transition.of("ease", 0.1.s))
-                    .thenIf(
-                        gifInputs.isEmpty(),
-                        Modifier
-                            .cursor(Cursor.Pointer)
-                            .styled(HoverHighlightStyle),
-                    )
-                    .thenIf(
-                        highlight && gifInputs.isEmpty(),
-                        HighlightModifier,
-                    ),
-                acceptTypes = listOf("image/*", "video/*"),
-                clickable = gifInputs.isEmpty(),
-                onDrag = { isEnter ->
-                    highlight = isEnter
-                },
-                onFilesSelected = { files ->
-                    coroutineScope.launch {
-                        files.forEach { file ->
-                            try {
-                                val mediaInfo = mutex.withLock {
-                                    worker.submit(
-                                        GifWorkerInput.MediaQuery,
-                                        Attachments {
-                                            add("file", file)
-                                        },
-                                    )
-                                }.content
-                                if (mediaInfo !is GifWorkerOutput.MediaQueryResult) {
-                                    throw createWrongOutputTypeException(mediaInfo)
-                                }
-                                gifInputs += GifInput(
-                                    file,
-                                    URL.createObjectURL(file),
-                                    mediaInfo.frameCount,
-                                    mediaInfo.isVideo,
-                                )
-                            } catch (t: Throwable) {
-                                console.error("Error processing file: ${file.name}", t.message ?: t)
-                                window.alert("Error processing file: ${file.name}")
-                            }
-                        }
-                    }
-                },
+                    .gap(1.cssRem),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                if (gifInputs.isEmpty()) {
-                    H1(
-                        Modifier
-                            .margin(autoLength)
-                            .fontSize(2.cssRem)
-                            .toAttrs(),
-                    ) {
-                        Text("Drop images here")
-                    }
-                } else {
-                    Div(
-                        Modifier
-                            .margin(autoLength)
-                            .gap(1.cssRem)
-                            .styled(DynamicGrid4)
-                            .toAttrs(),
-                    ) {
-                        gifInputs.forEachIndexed { index, file ->
-                            Box(
-                                Modifier
-                                    .fillMaxSize()
-                                    .backgroundColor(Color.lightgray)
-                                    .borderRadius(10.px)
-                                    .padding(10.px)
-                                    .onClick {
-                                        val removed = gifInputs.removeAt(index)
-                                        URL.revokeObjectURL(removed.url)
-                                    },
-                                contentAlignment = Alignment.Center,
-                            ) {
-                                if (file.isVideo) {
-                                    Video(
-                                        Modifier
-                                            .maxSize(100.percent)
-                                            .toAttrs {
-                                                attr("autoplay", "")
-                                                attr("muted", "")
-                                                attr("playsinline", "")
-                                                attr("loop", "")
-                                                ref { element ->
-                                                    /*
-                                                     * "muted" attribute doesn't work with
-                                                     * dynamically added video elements
-                                                     */
-                                                    element.muted = true
-                                                    onDispose {}
-                                                }
-                                            }
-                                    ) {
-                                        Source(attrs = {
-                                            attr("src", file.url)
-                                            attr("type", "video/mp4")
-                                        })
-                                    }
-                                } else {
-                                    Image(
-                                        file.url,
-                                        Modifier.maxSize(100.percent),
-                                    )
-                                }
-                            }
-                        }
+                ImageDropBox(gifInputs, worker, mutex)
+
+                ProgressArrow(
+                    currentProgress = encodedFrames,
+                    total = totalFrames,
+                )
+
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(1.cssRem)
+                        .borderRadius(10.px)
+                        .backgroundColor(Color.lightgray),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    if (resultUrl.isNotEmpty()) {
+                        Image(
+                            resultUrl,
+                            Modifier.maxSize(100.percent),
+                        )
                     }
                 }
             }
@@ -254,7 +161,7 @@ fun HomePage() {
                 Button(
                     onClick = {
                         if (gifInputs.isEmpty()) return@Button
-                        startTime = Clock.System.now()
+                        startTime = TimeSource.Monotonic.markNow()
                         encoding = true
                         encodedFrames = 0
                         coroutineScope.launch {
@@ -271,8 +178,7 @@ fun HomePage() {
                             )
                             resultUrl = URL.createObjectURL(blob)
                             encoding = false
-                            averageFps = totalFrames / (elapsedTime.inWholeMilliseconds / 1000.0)
-                            maxFps = max(maxFps, averageFps)
+                            fps = totalFrames / (elapsedTime.inWholeMilliseconds / 1000.0)
                         }
                     },
                     enabled = gifInputs.isNotEmpty(),
@@ -294,34 +200,36 @@ fun HomePage() {
                     },
                 )
 
+                val estimatedTimeRemaining = if (encoding) {
+                    val remainingFrames = totalFrames - encodedFrames
+                    if (fps > 0) {
+                        (remainingFrames / fps).seconds
+                    } else {
+                        null
+                    }
+                } else {
+                    Duration.ZERO
+                }
+                val estimatedTimeRemainingText = estimatedTimeRemaining
+                    ?.toString(DurationUnit.SECONDS, 0)
+                    ?: "Calculating..."
                 Text(
                     "Frames encoded: $encodedFrames/$totalFrames, " +
-                        "Average FPS: ${averageFps.format(2)}, " +
-                        "Max FPS: ${maxFps.format(2)}, " +
-                        "Elapsed time: $elapsedTime",
-                )
-            }
-
-            if (resultUrl.isNotEmpty()) {
-                Image(
-                    resultUrl,
-                    Modifier.maxHeight(500.px),
+                        "FPS: ${fps.format(2)}, " +
+                        "Elapsed time: $elapsedTime, " +
+                        "Estimated time remaining: $estimatedTimeRemainingText",
                 )
             }
         }
     }
 }
 
-private fun Number.format(decimals: Int): Double {
-    return toFixed(decimals).toDouble()
-}
-
-@OptIn(DelicateCoroutinesApi::class)
 @Composable
 private fun createWorker(mutex: Mutex): GifWorker {
     val worker = remember { GifWorker() }
     DisposableEffect(worker) {
         onDispose {
+            @OptIn(DelicateCoroutinesApi::class)
             GlobalScope.launch {
                 try {
                     withTimeout(10.seconds) {
@@ -336,6 +244,167 @@ private fun createWorker(mutex: Mutex): GifWorker {
         }
     }
     return worker
+}
+
+@Composable
+private fun ImageDropBox(
+    gifInputs: MutableList<GifInput>,
+    worker: GifWorker,
+    mutex: Mutex,
+) {
+    val coroutineScope = rememberCoroutineScope()
+    var highlight by remember { mutableStateOf(false) }
+
+    FileDropBox(
+        Modifier
+            .fillMaxSize()
+            .borderRadius(10.px)
+            .padding(30.px)
+            .backgroundColor(Color.gray)
+            .outline(width = 3.px, style = LineStyle.Dashed, color = Color.black)
+            .outlineOffset((-10).px)
+            .display(DisplayStyle.Flex)
+            .overflow(Overflow.Auto)
+            .transition(Transition.of("ease", 0.1.s))
+            .thenIf(
+                gifInputs.isEmpty(),
+                Modifier
+                    .cursor(Cursor.Pointer)
+                    .styled(HoverHighlightStyle),
+            )
+            .thenIf(
+                highlight && gifInputs.isEmpty(),
+                HighlightModifier,
+            ),
+        acceptTypes = listOf("image/*", "video/*"),
+        clickable = gifInputs.isEmpty(),
+        onDrag = { isEnter ->
+            highlight = isEnter
+        },
+        onFilesSelected = { files ->
+            coroutineScope.launch {
+                files.forEach { file ->
+                    try {
+                        val mediaInfo = mutex.withLock {
+                            worker.submit(
+                                GifWorkerInput.MediaQuery,
+                                Attachments {
+                                    add("file", file)
+                                },
+                            )
+                        }.content
+                        if (mediaInfo !is GifWorkerOutput.MediaQueryResult) {
+                            throw createWrongOutputTypeException(mediaInfo)
+                        }
+                        gifInputs += GifInput(
+                            file,
+                            URL.createObjectURL(file),
+                            mediaInfo.frameCount,
+                            mediaInfo.isVideo,
+                        )
+                    } catch (t: Throwable) {
+                        console.error("Error processing file: ${file.name}", t.message ?: t)
+                        window.alert("Error processing file: ${file.name}")
+                    }
+                }
+            }
+        },
+    ) {
+        if (gifInputs.isEmpty()) {
+            H1(
+                Modifier
+                    .margin(autoLength)
+                    .fontSize(2.cssRem)
+                    .toAttrs(),
+            ) {
+                Text("Drop images here")
+            }
+        } else {
+            Div(
+                Modifier
+                    .margin(autoLength)
+                    .gap(1.cssRem)
+                    .styled(DynamicGrid4)
+                    .toAttrs(),
+            ) {
+                gifInputs.forEachIndexed { index, file ->
+                    Box(
+                        Modifier
+                            .fillMaxSize()
+                            .backgroundColor(Color.lightgray)
+                            .borderRadius(10.px)
+                            .padding(10.px)
+                            .onClick {
+                                val removed = gifInputs.removeAt(index)
+                                URL.revokeObjectURL(removed.url)
+                            },
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        if (file.isVideo) {
+                            Video(
+                                Modifier
+                                    .maxSize(100.percent)
+                                    .toAttrs {
+                                        attr("autoplay", "")
+                                        attr("muted", "")
+                                        attr("playsinline", "")
+                                        attr("loop", "")
+                                        ref { element ->
+                                            /*
+                                             * "muted" attribute doesn't work with
+                                             * dynamically added video elements
+                                             */
+                                            element.muted = true
+                                            onDispose {}
+                                        }
+                                    }
+                            ) {
+                                Source(attrs = {
+                                    attr("src", file.url)
+                                    attr("type", "video/mp4")
+                                })
+                            }
+                        } else {
+                            Image(
+                                file.url,
+                                Modifier.maxSize(100.percent),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ProgressArrow(
+    currentProgress: Int,
+    total: Int,
+) {
+    FaArrowRight(
+        Modifier
+            .color(Colors.Transparent)
+            .background {
+                clip(BackgroundClip.Text)
+                image(
+                    linearGradient(LinearGradient.Direction.ToRight) {
+                        val progress = if (total > 0) {
+                            currentProgress * 100.0 / total
+                        } else {
+                            0.0
+                        }
+                        add(Colors.LightGreen, progress.percent)
+                        add(Colors.Black, progress.percent)
+                    }
+                )
+            },
+        size = IconSize.X8,
+    )
+}
+
+private fun Number.format(decimals: Int): Double {
+    return toFixed(decimals).toDouble()
 }
 
 private data class GifInput(
